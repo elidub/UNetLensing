2243f79dbab3010c505b7e8598261d0a0a2f94a0
Elias Dubbeldam <eliasd@test3.irc.sara.nl.irc.sara.nl>
2022-01-24 11:09:46 +0100

    First commit

---
+++ scripts/train.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+import torch, datetime, click
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
+
+import swyft
+from utils import *
+
+DEVICE = 'cuda'
+
+
+@click.command()
+@click.option("--m",    type=int, default = 12,  help="Exponent of subhalo mass.")
+@click.option("--nsub", type=int, default = 1,   help="Number of subhaloes.")
+@click.option("--nsim", type=int, default = 100, help="Number of simulations to run.")
+
+@click.option("--nmbins",  type=int, default = 2,   help="Number of mass bins.")
+
+@click.option("--lr",         type=float, default = 1e-3, help="Learning rate.")
+@click.option("--factor",     type=float, default = 1e-1, help = "Factor of Scheduler")
+@click.option("--patience",   type=int,   default = 5,    help = "Patience of Scheduler")
+@click.option("--max_epochs", type=int,   default = 30,   help = "Max number of epochs.")
+
+
+
+# m = 0
+# nsub = 3
+# nsim = 200
+
+# nmbins = 2
+
+# lr = 1e-3
+# factor = 1e-1
+# patience = 5
+# max_epochs = 1
+
+
+def run(m, nsub, nsim, nmbins, lr, factor, patience, max_epochs):
+    time_start = datetime.datetime.now()
+    
+    # Set definitions (should go to click)
+    system_name = "ngc4414"
+    
+    # Set utilities
+    sim_name, sim_path = get_sim_path(m, nsub, nsim, system_name)
+    store = swyft.Store.load(path=sim_path)
+    print(f'Store has {len(store)} simulations.')
+    
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)  # HACK
+    config = get_config(system_name, str(nsub), str(m))
+    torch.set_default_tensor_type(torch.FloatTensor)
+
+    prior, n_pars, lows, highs = get_prior(config)
+    L = config.kwargs["defs"]["nx"]
+    print(f'Image has L = {L}.')
+    
+    dataset = swyft.Dataset(nsim, prior, store)#, simhook = noise)
+
+    # Train network
+    mre_name, mre_path = get_mre_path(sim_name, nmbins, lr, factor, patience)
+    print(f'Training {mre_name}!')
+    
+    marginal_indices, _ = swyft.utils.get_corner_marginal_indices(n_pars)
+
+    network = get_custom_marginal_classifier(
+        observation_transform = CustomObservationTransform('image', {'image': (L, L)}),
+        marginal_indices = marginal_indices,
+        L = L,
+        nmbins = nmbins, 
+        lows = lows,
+        highs = highs,
+        marginal_classifier = CustomMarginalClassifier,
+    )
+
+    mre = swyft.MarginalRatioEstimator(
+        marginal_indices = marginal_indices,
+        network = network,
+        device = DEVICE,
+    )
+
+    _ = mre.train(dataset, max_epochs = max_epochs)
+
+    mre.save(mre_path)
+    
+    print('Done!')
+    print(f"Total training time is {str(datetime.datetime.now() - time_start).split('.')[0]}!")
+    
+if __name__ == "__main__":
+    run()
---
+++ scripts/utils.py
@@ -0,0 +1,167 @@
+import os, torch, pyro, numpy as np
+
+torch.set_default_tensor_type(torch.cuda.FloatTensor)  # HACK
+
+from clipppy import load_config, Clipppy
+from clipppy.patches import torch_numpy
+from ruamel.yaml import YAML
+
+import swyft
+from pyrofit.lensing.distributions import get_default_shmf
+from classifier import *
+
+DEVICE = 'cuda'
+
+
+def get_custom_marginal_classifier(
+    observation_transform,
+    marginal_indices: tuple,
+    L,
+    nmbins, 
+    lows,
+    highs,
+    marginal_classifier,
+) -> torch.nn.Module:
+    
+    n_observation_features = observation_transform.n_features
+    
+    parameter_transform = CustomParameterTransform(nmbins, L, lows, highs)
+
+    marginal_classifier = marginal_classifier(
+        len(marginal_indices),
+        n_observation_features,
+        nmbins
+    )
+
+    return swyft.networks.Network(
+        observation_transform,
+        parameter_transform,
+        marginal_classifier,
+    )
+
+def get_config(system_name: str, nsub: str = '', m: str = '') -> Clipppy:
+    """
+    Get configuration
+    """
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)  # HACK
+
+    SOURCE_DIR = '../../../mock_data/sources'
+        
+    source_name = f'{system_name}.npy'
+    config = load_config(f'../configs/config_nsub{nsub}_m{m}.yaml', base_dir=SOURCE_DIR)
+
+    torch.set_default_tensor_type(torch.FloatTensor)  # HACK
+    return config
+
+
+def get_prior(CONFIG):
+    config = CONFIG
+    main = config.umodel.alphas["main"]
+    prior_p_sub = main.sub.pos_sampler.base_dist
+    m_sub_grid = main.sub.mass_sampler.y
+
+    nsub = main.sub.nsub
+    z_lens = config.kwargs['defs']['z_lens']
+
+    lows = np.array([
+            prior_p_sub.low[0].item(),
+            prior_p_sub.low[1].item(),
+            m_sub_grid.min().log10().item(),
+        ])
+    highs = np.array([
+            prior_p_sub.high[0].item(),
+            prior_p_sub.high[1].item(),
+            m_sub_grid.max().log10().item(),
+        ])
+    
+    uniform = torch.distributions.Uniform(torch.tensor(lows[:-1]), torch.tensor(highs[:-1]))
+    shmf = get_default_shmf(z_lens = z_lens, log_range = (lows[-1], highs[-1]))
+
+    parameter_dimensions = [2, 1]*nsub
+    n_pars = sum(parameter_dimensions)
+    
+    prior = swyft.Prior.composite_prior(
+        cdfs=list(map(swyft.Prior.conjugate_tensor_func, [uniform.cdf, shmf.cdf]*nsub)),
+        icdfs=list(map(swyft.Prior.conjugate_tensor_func, [uniform.icdf, shmf.icdf]*nsub)),
+        log_probs=list(map(swyft.Prior.conjugate_tensor_func, [uniform.log_prob, shmf.log_prob]*nsub)),
+        parameter_dimensions=parameter_dimensions,
+    )
+
+    return prior, n_pars, lows, highs
+
+
+def simul(v, config: Clipppy):
+    """
+    Fix values for main lens and source parameters from config and put
+    in a subhalo with the specified position and mass.
+
+    Arguments
+    - v: array containing x_sub, y_sub.
+
+    Returns
+    - Numpy array.
+    """
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)  # HACK
+    
+    from pyrofit.lensing.utils import get_meshgrid  # import here due to HACKs
+    nx = config.kwargs["defs"]["nx"]
+    ny = config.kwargs["defs"]["ny"]
+    res = config.kwargs["defs"]["res"]
+    nsub = config.umodel.alphas["main"].sub.nsub
+    X, Y = config.umodel.X.clone(), config.umodel.Y.clone()
+    # Upsample image
+    upsample = 10
+    config.umodel.coerce_XY(*get_meshgrid(res / upsample, nx * upsample, ny * upsample))
+        
+    if not torch.is_tensor(v):
+        v = torch.tensor(v)
+
+    x_sub, y_sub, m_sub = v.view(-1,3).T.to(DEVICE)
+    xy_sub = torch.stack((x_sub, y_sub)).T
+    
+    d_m_sub = pyro.distributions.Delta(m_sub)
+    d_p_sub = pyro.distributions.Delta(xy_sub).to_event(1)
+
+
+    def _guide():
+        # Sample subhalo position
+        guide_sample = {
+            "main/sub/p_sub": pyro.sample("main/sub/p_sub", d_p_sub),
+            "main/sub/m_sub": pyro.sample("main/sub/m_sub", d_m_sub),
+        }
+
+        return guide_sample
+    
+    result = {
+        "image": config.ppd(guide=_guide)["model_trace"]
+        .nodes["mu"]["value"]
+        .detach()
+        .numpy()
+    }
+    
+    # Restore coarse grid
+    config.umodel.coerce_XY(X, Y)
+    # Downsample image
+    averager = torch.nn.AvgPool2d((upsample, upsample))
+    result['image'] = (averager(torch.tensor(result['image']).unsqueeze(0).unsqueeze(0)).squeeze(0).squeeze(0))
+
+    torch.set_default_tensor_type(torch.FloatTensor)  # HACK
+    return result
+
+def noise(obs, _=None, sigma_n=0.1):
+    image = obs["image"]
+    eps = np.random.randn(*image.shape) * sigma_n
+    return {"image": image + eps}
+
+def get_sim_path(m, nsub, nsim, system_name):
+    sim_name = f'_M_m{m}_nsub{nsub}_nsim{nsim}'
+    sim_path = f'/nfs/scratch/eliasd/store{sim_name}.zarr' 
+    print(f'Store {sim_name} exists!') if os.path.exists(sim_path) else print('Store does not exist!')
+    return sim_name, sim_path
+
+def get_mre_path(sim_name, nmbins, lr, factor, patience, save_dir = '../data/mre'):
+    save_id = f'nmbins{nmbins}_lr{np.log10(lr)}_fac{np.log10(factor)}_pat{patience}'
+    mre_name = f'UNet{sim_name}_{save_id}.pt'
+    mre_path = os.path.join(save_dir, mre_name)
+    return mre_name, mre_path
+
